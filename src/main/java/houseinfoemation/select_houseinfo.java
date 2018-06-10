2d58cbe1a202d337d9e3e953e59f20330f126f6c
徐浩杰 <928675426@qq.com>
2018-06-10 18:47:56 +0800

    2

---
+++ src/main/java/houseinfoemation/select_houseinfo.java
@@ -0,0 +1,43 @@
+package houseinfoemation;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class select_houseinfo {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					select_houseinfo window = new select_houseinfo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public select_houseinfo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
